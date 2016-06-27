432e73f9ef02520743e6b9e245d06d6c1b623d1c
Cristian Daniel Marquez <c.marquez@globant.com>
2016-06-27 12:33:19 -0500

    Added jQuerry, flipped and front-card styles

--- www/js/controllers/asignaturas.js
+++ www/js/controllers/asignaturas.js
@@ -3,11 +3,17 @@ angular.module('starter.controllers')
 .controller('AsignaturasController', ['$scope', '$stateParams', 'getService', function($scope, $stateParams, getService) {
   $scope.perfil_id = parseInt($stateParams.perfil_id,10);
   $scope.perfilName = $stateParams.perfil_name || "Asignaturas";
+  $scope.flippedCard = true;
 
   getService
     .getData("/api_asignatura_rutas", {perfile_id: $scope.perfil_id})
     .success(function(response) {
       $scope.asignaturas = response.data;
     });
+
+  $scope.flipper = function($event) {
+    var $element = angular.element($event.currentTarget);
+    $element.toggleClass('flip');
+  };
 
 }]);
