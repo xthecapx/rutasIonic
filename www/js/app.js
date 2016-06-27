0959a8365007b172da6ebfc52725fc8598a5d66f
Cristian Casa <xthecapx@gmail.com>
2016-06-27 18:35:48 -0500

    custom tooltip added, removed not needed tooltips

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -72,25 +72,4 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/home');
-});
-
-
-angular.module('starter')
-
-.directive('tooltip', function () {
-    return {
-        restrict: 'C',
-        link: function (scope, element, attrs) {
-            if (attrs.title) {
-                var $element = $(element);
-                $element.attr("title", attrs.title);
-                $element.tooltipster({
-                    animation: attrs.animation,
-                    trigger: "click",
-                    touchDevices: true,
-                    contentAsHTML: true
-                });
-            }
-        }
-    };
 });
---
+++ www/js/directives/tooltip.js
@@ -0,0 +1,61 @@
+angular.module('starter.directives', [])
+
+.directive('tooltip', function ($document, $compile) {
+  return {
+     restrict: 'A',
+     scope: true,
+     link: function (scope, element, attrs) {
+
+       var tip = $compile('<div ng-class="tipClass">{{ text }}<div class="tooltip-arrow"></div></div>')(scope),
+           tipClassName = 'tooltip',
+           tipActiveClassName = 'tooltip-show';
+
+       scope.tipClass = [tipClassName];
+       scope.text = attrs.tooltip;
+
+       if(attrs.tooltipPosition) {
+         scope.tipClass.push('tooltip-' + attrs.tooltipPosition);
+       }
+       else {
+        scope.tipClass.push('tooltip-down');
+       }
+
+       $document.find('.tooltip-parent').append(tip);
+
+       element.bind('click', function (e) {
+         tip.addClass(tipActiveClassName);
+
+         var pos = e.target.getBoundingClientRect(),
+             offset = tip.offset(),
+             tipHeight = tip.outerHeight(),
+             tipWidth = tip.outerWidth(),
+             elWidth = pos.width || pos.right - pos.left,
+             elHeight = pos.height || pos.bottom - pos.top,
+             tipOffset = 10;
+
+         if(tip.hasClass('tooltip-right')) {
+           offset.top = pos.top - (tipHeight / 2) + (elHeight / 2);
+           offset.left = pos.right + tipOffset;
+         }
+         else if(tip.hasClass('tooltip-left')) {
+           offset.top = pos.top - (tipHeight / 2) + (elHeight / 2);
+           offset.left = pos.left - tipWidth - tipOffset;
+         }
+         else if(tip.hasClass('tooltip-down')) {
+           offset.top = pos.top + elHeight + tipOffset;
+           offset.left = pos.left - (tipWidth / 2) + (elWidth / 2);
+         }
+         else {
+           offset.top = pos.top - tipHeight - tipOffset;
+           offset.left = pos.left - (tipWidth / 2) + (elWidth / 2);
+         }
+
+         tip.offset(offset);
+       });
+
+       tip.bind('click', function () {
+         tip.removeClass(tipActiveClassName);
+       });
+     }
+   }
+});
