c164fd97548e8a1dfa678efbef52a97bd94757b8
Cristian Daniel Marquez <c.marquez@globant.com>
2016-07-07 15:40:03 -0500

    Added loader to perfiles, added loaders scss library

--- www/js/controllers/perfiles.js
+++ www/js/controllers/perfiles.js
@@ -2,10 +2,12 @@ angular.module('starter.controllers')
 
 .controller('PerfilesController', ['$scope', '$stateParams', 'getService', 'ApiEndpoint', function($scope, $stateParams, getService, ApiEndpoint) {
   $scope.programa_id = parseInt($stateParams.programa_id,10);
+  $scope.loading = true;
 
   getService
     .getData("/api_perfiles", {programa_id: $scope.programa_id})
     .success(function(response) {
+      $scope.loading = false;
       $scope.perfiles = response.data;
     });
 
