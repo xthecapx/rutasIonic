8d714d801a2434013fd10c5563fed3a649c9547d
Cristian Daniel Marquez <c.marquez@globant.com>
2016-09-01 13:07:20 -0500

    Added sort order on afinidades and updated styles

--- www/js/controllers/afinidades_detalle.js
+++ www/js/controllers/afinidades_detalle.js
@@ -1,9 +1,14 @@
 angular.module('starter.controllers')
 
-.controller('AfinidadesDetalleController', ['$scope', '$stateParams', 'getService', 'ApiEndpoint', 'AuthenticationService', function($scope, $stateParams, getService, ApiEndpoint, login) {
+.controller('AfinidadesDetalleController', ['$scope', '$stateParams', '$filter', 'getService', 'ApiEndpoint', 'AuthenticationService', function($scope, $stateParams, $filter, getService, ApiEndpoint, login) {
 
   $scope.idProgramaOrigen = parseInt($stateParams.idProgramaOrigen, 10);
   $scope.loading = true;
+  $scope.reverse = true;
+
+  $scope.sorterFunc = function(item){
+    return parseInt(item.errorOrigenDestino);
+  };
 
   getService
     .getData("/rutas/api_afinidades_destino", {idProgramaOrigen: $scope.idProgramaOrigen})
