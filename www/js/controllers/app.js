a37c824e0f6687ef23f6cf4c503aaa2959714380
Cristian Daniel Marquez <c.marquez@globant.com>
2016-09-27 15:15:13 -0500

    Updated variables for deploy server

--- www/js/controllers/app.js
+++ www/js/controllers/app.js
@@ -1,11 +1,11 @@
 angular.module('starter.controllers', [])
 
 .constant('ApiEndpoint', {
-  //url: 'http://retalapp.com/rutas', //Deploy app to mobile
+  //url: 'http://www.pregrado.unal.edu.co/rutas/admin', //Deploy app to mobile
   url: 'api', //Ionic serve to test real api rest
   //url: '', // gulp serve to run Mock data
   client_id: "android",
-  client_secret: "O20wgetsGF82eFTwTVA2qWFAcxGhiq0x"
+  client_secret: "kKJYWBfTguREWTna7eYkzeqtmTYfhGto"
 })
 
 .controller('AppController', ['$scope', function($scope) {
