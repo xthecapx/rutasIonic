da07ce173e519d09a0ca13510cabfd79a16aee11
Im Cap Marquez <Im Cap Marquez>
2017-01-25 13:51:28 -0500

    Added selects to select a category and a video in tutoriales

--- www/js/controllers/tutoriales.js
+++ www/js/controllers/tutoriales.js
@@ -2,12 +2,30 @@ angular.module('starter.controllers')
 
   .controller('TutorialesController', ['$scope', '$http', 'ApiEndpoint', 'AuthenticationService', '$sce', '$timeout', function ($scope, $http, ApiEndpoint, login, $sce, $timeout) {
 
+    var selectConstructor = function (names) {
+      angular.forEach(names, function(name, key) {
+        $scope[name] = {
+          repeatSelect: null,
+          availableOptions: [],
+        };
+      });
+    };
+
     $scope.user = login.getUser();
 
     $scope.$on("update-user", function () {
       $scope.user = login.getUser();
     });
 
+    selectConstructor(["categoria", "video"]);
+
+    $scope.categoria.availableOptions = [{
+      id: 1,
+      name: "PAPA"
+    }, {
+      id: 2,
+      name: "Educación"
+    }];
 
     $scope.state = null;
     $scope.API = null;
@@ -19,7 +37,6 @@ angular.module('starter.controllers')
 
     $scope.onCompleteVideo = function () {
       $scope.isCompleted = true;
-
       $scope.currentVideo++;
 
       if ($scope.currentVideo >= $scope.videos.length) {
@@ -28,6 +45,26 @@ angular.module('starter.controllers')
 
       $scope.setVideo($scope.currentVideo);
     };
+
+    $scope._selectVideo = {
+      show: function() {
+        $scope.loader.video = true;
+        $scope.disable.video = false;
+      },
+      hide: function() {
+        $scope.loader.video = false;
+        $scope.disable.video = true;
+      }
+    };
+
+    $scope.disable = {
+      video: false,
+    };
+
+    $scope.loader = {
+      video: false,
+    };
+
 
     $scope.videos = [
       {
@@ -65,5 +102,33 @@ angular.module('starter.controllers')
       $scope.config.sources = $scope.videos[index].sources;
       $timeout($scope.API.play.bind($scope.API), 100);
     };
+
+    /* Watchers */
+    $scope.$watch('[categoria.repeatSelect, video.repeatSelect]', function (newValue, oldValue) {
+      if (newValue[0] !== oldValue[0]) {
+        $scope._selectVideo.show();
+
+        $scope.video.availableOptions = [{
+          id: 0,
+          name: "Pale Blue Dot"
+        }, {
+          id: 1,
+          name: "Big Buck Bunny"
+        }];
+        /*httpCall({
+          uri: "/rutas/api_facultades",
+          parameters: {sede_id: $scope.sede.repeatSelect},
+          selectOptions: $scope.facultad.availableOptions,
+          idValue: "facultad_id",
+          nameValue: "facultad",
+        });*/
+        $scope._selectVideo.hide();
+
+      }
+
+      if (newValue[1] !== oldValue[1]) { 
+        $scope.setVideo(newValue[1]);
+      }
+    }, true);
 
   }]);
